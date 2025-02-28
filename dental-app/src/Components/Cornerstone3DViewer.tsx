6a34fa740903c32bf81eef4e5d9d1c38e4828c35
Omar Adel ElBehairy <omaradelelbehairy@gmail.com>
2025-02-28 14:29:35 +0200

    the 3 views appear with no bug

--- dental-app/src/Components/Cornerstone3DViewer.tsx
+++ dental-app/src/Components/Cornerstone3DViewer.tsx
@@ -89,6 +89,11 @@ const Cornerstone3DViewer = () => {
         const [ctImageIds, ptImageIds] = await Promise.all([
           createImageIdsAndCacheMetaData({
             StudyInstanceUID,
+            SeriesInstanceUID: '1.3.6.1.4.1.14519.5.2.1.7009.2403.226151125820845824875394858561',
+            wadoRsRoot,
+          }),
+          createImageIdsAndCacheMetaData({
+            StudyInstanceUID,
             SeriesInstanceUID: '1.3.6.1.4.1.14519.5.2.1.7009.2403.879445243400782656317561081015',
             wadoRsRoot,
           })
