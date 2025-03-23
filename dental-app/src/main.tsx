3357f4dfd6dba7af66ef0e36938333b8fa44aef2
Omar Adel ElBehairy <omaradelelbehairy@gmail.com>
2025-03-23 02:02:32 +0200

    removed any thing about tailwind.css + adjusted sidebar

--- dental-app/src/main.tsx
+++ dental-app/src/main.tsx
@@ -1,20 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
-// import Nifti from './Nifti.tsx'
-import './index.css'
-// import Cornerstone3DViewer from "./Components/Cornerstone3DViewer";
-import VolumeViewer3D from './Components/VolumeViewer3D.tsx'
-// MultiplanarConstruction.tsx this file is to fix the bug in sagittal, cronal and axial views but it doesn't work yet
-// import CornerstoneVolumeViewer from './Components/MultiplanarConstruction.tsx'
-import { SideBar } from './Components/sidebar.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import VolumeViewer3D from './Components/VolumeViewer3D.tsx';
+import { SideBar } from './Components/SideBar.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="relative">
-      {/* Main content area */}
-      <VolumeViewer3D />
+    <div className="app-container">
       <SideBar />
+      <div className="content-container">
+        <VolumeViewer3D />
+      </div>
     </div>
   </React.StrictMode>
 );
