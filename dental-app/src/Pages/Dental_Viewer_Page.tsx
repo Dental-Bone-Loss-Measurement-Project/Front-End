c8e52a10562a50f812f16d2bee8d34abffb6744e
Omar Adel Hassan <omaradelhassan12@gmail.com>
2025-04-06 15:46:57 +0200

    Update

--- dental-app/src/Pages/Dental_Viewer_Page.tsx
+++ dental-app/src/Pages/Dental_Viewer_Page.tsx
@@ -5,21 +5,21 @@ import SagittalView from "../Components/Dental_Viewer/SagittalView";
 
 const DentalViewerPage = () => {
   return (
-    <div className="grid grid-cols-2 grid-rows-2 gap-4 h-full">
+    <div className="grid grid-cols-2 grid-rows-2 gap-1 w-full h-full">
       {/* 3D Viewer */}
-      <div className="border border-gray-300 overflow-x-auto overflow-y-auto h-full">
+      <div className="border border-gray-300 overflow-hidden h-full">
         <VolumeViewer3D />
       </div>
       {/* Sagittal view */}
-      <div className="border border-gray-300 overflow-x-auto overflow-y-auto h-full">
+      <div className="border border-gray-300 overflow-hidden h-full">
         <SagittalView />
       </div>
       {/* Coronal view */}
-      <div className="border border-gray-300 overflow-x-auto overflow-y-auto h-full">
+      <div className="border border-gray-300 overflow-hidden h-full">
         <CoronalView />
       </div>
       {/* Axial view */}
-      <div className="border border-gray-300 overflow-x-auto overflow-y-auto h-full">
+      <div className="border border-gray-300 overflow-hidden h-full">
         <AxialView />
       </div>
     </div>
