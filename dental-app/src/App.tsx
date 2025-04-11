19f462355cb012c3db7f2427b706be8b3f83361e
Omar Adel ElBehairy <omaradelelbehairy@gmail.com>
2025-04-11 19:18:44 +0200

    removed react-tailwind-css folder + adjusted crosshair

--- dental-app/src/App.tsx
+++ dental-app/src/App.tsx
@@ -1,6 +1,9 @@
 import Header from "./Components/Header";
 import { SideBar } from "./Components/SideBar";
-import DentalViewerPage from "./Pages/Dental_Viewer_Page";
+// import DentalViewerPage from "./Pages/Dental_Viewer_Page";
+// import MultiViewer from "./Components/MultiViewer";
+import Crosshairs from "./Components/Crosshairs";
+// import Crosshairs from "./Components/Crosshairs_gpt";
 
 function App() {
   return (
@@ -12,7 +15,9 @@ function App() {
         <SideBar />
         {/* Main content area */}
         <div className="content-container flex-1 p-4">
-          <DentalViewerPage />
+          {/* <DentalViewerPage /> */}
+          {/* <MultiViewer/> */}
+          <Crosshairs/>
         </div>
       </div>
     </div>
--- dental-app/src/Components/Dental_Viewer/AxialView.tsx
+++ dental-app/src/Components/Dental_Viewer/AxialView.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef } from 'react';
-import { Enums } from '@cornerstonejs/core';
 
 interface Props {
   renderingEngineId: string;
